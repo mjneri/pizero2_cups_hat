b0d404390adaad72f9da9428fa2e8c65c9181bba
mjneri <michael.neri@eee.upd.edu.ph>
2024-03-14 16:15:22 +0800

    test commit 2

--- src/cups_hat_display.py
+++ src/cups_hat_display.py
@@ -430,5 +430,5 @@ if __name__ == '__main__':
     except SystemExit:
         #TODO: Replace or remove this SystemExit processing later.
         #NOTE: SystemExit is the exception result of calling exit()
-        print("Byeeee")
+        print("Byeee")
         cups_hat.display_shutdown()
